47a8bba19bf28e10ec598021516340b69993c430
userAP5AgPJU <userAP5AgPJU@example.com>
2026-07-16 14:36:17 +0400

    test: python sanity check

--- app/main.py
+++ app/main.py
@@ -1,5 +1,22 @@
+from pathlib import Path
+import json
+
 def main() -> None:
-    print("Cyber Automation Triage")
+    alert_path = Path("examples/alert.json")
+
+    with alert_path.open("r", encoding="utf-8") as file:
+        alert = json.load(file)
+    print()
+    print("====JSON====")
+    print(alert)
+    print("============")
+    print()
+    print("==Objects===")
+    print(alert["alert_id"])
+    print(alert["source_ip"])
+    print(alert["domain"])
+    print("============")
+    print()
 
 if __name__ == "__main__":
     main()
